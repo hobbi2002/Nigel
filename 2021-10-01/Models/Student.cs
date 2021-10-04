e5ec3734dbca567b887053e41ff4f7be1ca57b10
702-6 <702-6@702-6>
2021-10-04 19:51:25 +0200

    Добавьте файлы проекта.

---
+++ 2021-10-01/Models/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_10_01
+{
+    class Student
+    {
+        private int age;
+        private int[][] mas = new int[3][];
+        public string FirstName {get; set;}
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Group { get; set; }
+        public int Age {
+            get { return age; }
+            set
+            {
+                if (value >= 0) age = value;
+                else age = 0;
+            }
+        }
+
+        public Student(string firstName, string name, string lastName, string group, int age, int[] m1, int[] m2, int[] m3)
+        {
+            FirstName = firstName;
+            Name = name;
+            LastName = lastName;
+            Group = group;
+            Age = age;
+            mas[0] = m1;
+            mas[1] = m2;
+            mas[2] = m3;
+        }
+
+        public int getMark(int i, int j)
+        {
+            return mas[i][j];
+        }
+
+        public void setMark(int i, int j, int mark)
+        {
+            mas[i][j] = mark;
+        }
+
+        public double getAvg(int i)
+        {
+            double S = 0;
+            for (int j = 0; j < mas[i].Length; j++)
+            {
+                S += mas[i][j];
+            }
+            return S / mas[i].Length;
+        }
+
+        public override string ToString()
+        {
+            string s = string.Empty;
+            s = FirstName + " " + Name + " " + LastName + " гр." + Group + " Возраст " + Age+"\n";
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = 0; j < mas[i].Length; j++)
+                {
+                    s += mas[i][j] + " ";
+                }
+                s += getAvg(i)+"\n";
+            }
+            return s;
+        }
+    }
+}
---
+++ 2021-10-01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2021_10_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[2];
+            for (int i = 0; i < students.Length; i++)
+            {
+                string firstname = Console.ReadLine();
+                string name = Console.ReadLine();
+                string lastname = Console.ReadLine();
+                string group = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+                Random rnd = new Random();
+                int[] m1 = new int[rnd.Next(3, 6)];
+                int[] m2 = new int[rnd.Next(3, 6)];
+                int[] m3 = new int[rnd.Next(3, 6)];
+                for (int j = 0; j < m1.Length; j++)
+                {
+                    m1[j] = rnd.Next(2, 6);
+                }
+                for (int j = 0; j < m2.Length; j++)
+                {
+                    m2[j] = rnd.Next(2, 6);
+                }
+                for (int j = 0; j < m3.Length; j++)
+                {
+                    m3[j] = rnd.Next(2, 6);
+                }
+                students[i] = new Student(firstname, name, lastname, group, age, m1, m2, m3);
+            }
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i].ToString());
+            }
+
+        }
+    }
+}
